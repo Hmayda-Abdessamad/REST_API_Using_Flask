279aca265ec13c1a17ba265ebfdf4a381c0f7d1b
unknown <abdoessamadhmayda@gmail.com>
2023-10-04 22:52:21 +0200

    adding Swagger documentation

--- hello.py
+++ hello.py
@@ -1,15 +1,29 @@
 from flask import Flask
+from flask_restplus import Api, Resource
 
 app = Flask("My Hello World Application")
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# Define a namespace for your API
+ns = api.namespace('hello', description='Hello World Operations')
 
+@ns.route("/")
+class HelloWorld(Resource):
+    def get(self):
+        """Returns a Hello World message"""
+        return "Hello World!"
 
+# Generate the Swagger JSON file
+@api.documentation
+def custom_ui():
+    return {
+        'swagger': '2.0',
+        'info': {
+            'title': 'Hello World API',
+            'description': 'Sample API for Hello World',
+            'version': '1.0',
+        },
+    }
 
-#this checks if this is the main programm and it's not imported as a sudmodule 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-# When no port is specified, starts at default port 5000
--- products.py
+++ products.py
@@ -1,9 +1,10 @@
 from flask import Flask,jsonify,request;
-
+from flask_swagger_ui import get_swaggerui_blueprint;
+from flask_cors import CORS  # Import the CORS extension
 import json
 
 app=Flask("Product Server")
-
+CORS(app)
 products=[
     {'id':143,'name':"notebook",'price':40},
     {'id':144,'name':"Black Marker",'price':10} 
@@ -45,6 +46,22 @@ def remove_product(id):
     products.remove(product)
     return '', 204
 
+# Configure Swagger UI
+SWAGGER_URL = '/swagger'
+API_URL = 'http://127.0.0.1:5000/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Sample API"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route('/swagger.json')
+def swagger():
+    with open('swagger.json', 'r') as f:
+        return jsonify(json.load(f))
 
 if __name__=="__main__":
     app.run(debug=True)
