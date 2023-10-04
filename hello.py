088e2f16a23e2c2e7f4bf744f96f18b7ab5d4a95
unknown <abdoessamadhmayda@gmail.com>
2023-10-04 21:15:30 +0200

    change the example

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
-from flask import Flask;
-app=Flask(__name__)
+from flask import Flask
+
+app = Flask("My Hello World Application")
+
 @app.route("/")
-def hello_world():
-    return "hello"
+def hello():
+    return "Hello World!"
+
+
+
+#this checks if this is the main programm and it's not imported as a sudmodule 
+
+if __name__=="__main__":
+    app.run(debug=True)
+# When no port is specified, starts at default port 5000
